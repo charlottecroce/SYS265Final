92850f45c085a52d4391d632406cf6ee59939cf7
charlottecroce <90940521+charlottecroce@users.noreply.github.com>
2025-04-17 16:28:30 -0400

    Update SetDefaultShell.ps1

--- Powershell/SetDefaultShell.ps1
+++ Powershell/SetDefaultShell.ps1
@@ -1,4 +1,7 @@
+Set-ItemProperty "HKLM:\Software\Microsoft\Powershell\1\ShellIds" -Name ConsolePrompting -Value $true
+
 New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell `
   -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
 
   Restart-Service sshd
+
